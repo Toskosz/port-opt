c3fe5291f7162909a04502c281a53f3de8032282
TokarskiThiago <thiago.tokarski2001@gmail.com>
2023-08-03 08:02:02 -0300

    docs: :memo: data

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
 
+data = {}
 
+with open('./data/COTAHIST_D01082023.txt') as file:
+    for line in file:
+        code = line[12:24].strip()
+        price = line[108:121].strip()
+        price = int(price) / 100
+        data[code] = price
+
+
+    
